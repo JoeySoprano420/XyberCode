4c96ded2f141f117f58adfc9cf0275d0da7c5fd2
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-03-09 04:49:57 -0400

    Create expression_ast_node.rs

---
+++ expression_ast_node.rs
@@ -0,0 +1,11 @@
+#[derive(Debug)]
+enum Expr {
+    Number(i32),
+    Variable(String),
+    Add(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
+    Eq(Box<Expr>, Box<Expr>),
+    Ne(Box<Expr>, Box<Expr>),
+}
